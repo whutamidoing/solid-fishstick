9e46ec6969ad1a071be326bddb8b995204ef1071
whutamidoing <raybendalemagallon@gmail.com>
2025-03-07 20:12:08 +0800

    deploy to pages

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,22 @@
 function Banner() {
-  return (
-    <>
-      <div className="featured-banner"></div>
-    </>
-  );
+  return <></>;
 }
-
-export function Carousel() {}
 export function TopFeatured() {}
+
+export function Carousel() {
+  let cards = [
+    { pick: "top1", imgsrc: "../assets/images/20240505174153.png" },
+    { pick: "top2", imgsrc: "../assets/images/20240505174153.png" },
+    { pick: "top3", imgsrc: "../assets/images/20240505174153.png" },
+    { pick: "top4", imgsrc: "../assets/images/20240505174153.png" },
+  ];
+  <div className="featured-wrapper">
+    {cards.map((card) => (
+      <div className="card" key={card.pick}>
+        <img src={card.imgsrc} />
+      </div>
+    ))}
+    ;
+  </div>;
+}
 export default Banner;
