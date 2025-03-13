09d548b65326dfccc560fa31e297125d87ad01d4
whutamidoing <negispringfield23@gmail.com>
2025-03-13 23:48:04 +0800

    user css

--- src/UserProfile.tsx
+++ src/UserProfile.tsx
@@ -1,11 +1,15 @@
 import users from "./data/users.json";
 import { useState } from "react";
+import HomeIcon from "./assets/icons/home.svg?react";
+import { Link } from "react-router-dom";
 
 interface User {
   username: string;
   email: string;
   profBanner: string;
   profPicture: string;
+  post: number;
+  bookmarks: Array<string>;
 }
 
 function UserProfile() {
@@ -18,7 +22,7 @@ function UserProfile() {
         className="profile-banner"
         style={{ backgroundImage: `url(${curruser.profBanner})` }}
       ></div>
-      <div className="user-details">
+      <div className="user-container">
         <div
           className="profile-picture"
           style={{ backgroundImage: `url(${curruser.profPicture})` }}
@@ -27,6 +31,20 @@ function UserProfile() {
           <span>{curruser.username}</span>
           <span className="email">{curruser.email}</span>
         </div>
+      </div>
+      <div className="user-stats">
+        <div>
+          <span>Contributions: {curruser.post.toString()}</span>
+          <span>Follows: {curruser.bookmarks.length.toString()}</span>
+        </div>
+        <div className="home">
+          <a href="../">
+            <HomeIcon />
+          </a>
+        </div>
+      </div>
+      <div id="Bookmarks">
+        <h1>Saved</h1>
       </div>
     </>
   );
