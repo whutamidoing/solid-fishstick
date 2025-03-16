938644855771f3bc6a98a3f3ee4a23ea6bf75c0f
whutamidoing <negispringfield23@gmail.com>
2025-03-16 22:57:08 +0800

    pages media correction

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/solid-fishstick">
+    <BrowserRouter basename="/solid-fishstick/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
