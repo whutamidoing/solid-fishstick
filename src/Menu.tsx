5af0c4f36a83745479a95977a94a1df04df8adc2
whutamidoing <negispringfield23@gmail.com>
2025-03-16 21:16:41 +0800

    Login Form

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,11 +1,24 @@
 import Banner from "./components/Banner";
 import Sidebar from "./components/Sidebar";
 import ArticleMenu from "./components/ArticleMenu";
+import LoginForm from "./components/Login";
+import { useState } from "react";
 
 function Menu() {
+  const [isLoginActive, setIsLoginActive] = useState(false);
+
   return (
     <>
-      <div className="columns">
+      <LoginForm setIsLoginActive={setIsLoginActive} />
+      <div
+        className="columns"
+        style={{
+          overflow: isLoginActive ? "hidden" : "auto",
+          height: isLoginActive ? "100vh" : "fit-content",
+          pointerEvents: isLoginActive ? "none" : "auto",
+          filter: isLoginActive ? "blur(10px)" : "",
+        }}
+      >
         <Sidebar />
         <div className="main-content">
           <Banner />
